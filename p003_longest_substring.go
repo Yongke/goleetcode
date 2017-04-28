678e59cd20edb5bbf83389db89e582e684cce927
Wang Yongke <wangyongke@gmail.com>
2017-04-28 11:03:07 +0800

    improve p003 - not good

--- p003_longest_substring.go
+++ p003_longest_substring.go
@@ -1,32 +1,27 @@
 package goleetcode
 
-import (
-	"bytes"
-)
-
 func lengthOfLongestSubstring(s string) int {
 	m := make(map[byte]int)
 	maxsublen := 0
 	sublen := 0
-	bstmp := []byte{}
 
 	for i := 0; i < len(s); i++ {
 		val := s[i]
-		if _, exist := m[val]; exist {
+		if last_i, exist := m[val]; exist {
 			if sublen > maxsublen {
 				maxsublen = sublen
 			}
-			parts := bytes.Split(bstmp, []byte{val})
-			for _, valOfPart1 := range parts[0] {
-				delete(m, valOfPart1)
+
+			m = make(map[byte]int)
+			for j := last_i + 1; j <= i; j++ {
+				m[s[j]] = j
 			}
-			sublen = len(parts[1]) + 1
-			bstmp = append(parts[1], val)
+			sublen = i - last_i
 			continue
 		}
-		m[val] = 0
+
+		m[val] = i
 		sublen += 1
-		bstmp = append(bstmp, val)
 	}
 	if sublen > maxsublen {
 		maxsublen = sublen
